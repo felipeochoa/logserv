0315b4686e70b2be75bc41f51d16a5a38599d0fc
Felipe <user@server>
2013-10-10 16:34:31 -0500

    chagned recv_until to recv_line and added timeout and max_line_len parameters

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import json
 import logging.handlers
+import socket
+import time
 
 from . import ProtocolError, VersionMismatchError
 
@@ -18,9 +20,14 @@ class SocketForwarder(logging.handlers.SocketHandler):
     def __init__(self, host, port=None, **kwargs):
         if port is not None:
             raise TypeError("%s does not take a 'port' argument")
+    max_line_length = 10240
         self.shook_hands = False
         self.kwargs = kwargs
         super().__init__(self, host, port)
+        if timeout is None:
+            self.timeout = socket.getdefaulttimeout()
+        else:
+            self.timeout = timeout
 
     def createSocket(self):
         """
@@ -34,24 +41,32 @@ class SocketForwarder(logging.handlers.SocketHandler):
             data = data.encode('UTF-8')
         self.send(data)
 
-    def recv_until(self, terminator, decode=True):
-        resp = ['']
-        if decode:
-            terminator = terminator.encode('UTF-8')
+    def recv_line(self, decode=True):
+        resp = [b'']
+        resp_len = 0
+        terminator = '\n'.encode('UTF-8')
+        start = time.time()
         while terminator not in resp[-1]:
-            resp.append(self.sock.recv(128))
+            if self.timeout is not None and time.time() > start + self.timeout:
+                raise socket.timeout
+            elif resp_len > self.max_line_length:
+                raise ProtocolError("a line of length < %d",
+                                    "too many bytes")
+            data = self.sock.recv(1024)
+            resp.append(data)
+            resp_len += len(data)
         resp = b''.join(resp)
         if decode:
             resp = resp.decode('UTF-8')
-        resp, other = resp.split(terminator)
+        other = resp.split('\n', 1)[1]
         if other:
             raise ProtocolError("a %s-terminated message" % terminator,
-                                other)
+                                resp)
         return resp
 
     def doHandshake(self):
-        resp = self.recv_until('\n')
         self.sendtext('HELLO %s\n' % self.version_str)
+        resp = self.recv_line()
         if not resp.startswith('HELLO '):
             raise ProtocolError('"HELLO <version>\n"', resp)
         elif resp[6:] != '1.0\n':
@@ -60,8 +75,8 @@ class SocketForwarder(logging.handlers.SocketHandler):
         params = {'--level': self.level}
         params.update(self.kwargs)
         param_json = json.dumps(params)
-        resp = self.recv_until('\n')
         self.sendtext('IDENTIFY %s' % param_json)
+        resp = self.recv_line()
         if resp != 'OK\n':
             raise ProtocolError("'OK\n'", resp)
         self.shook_hands = True
