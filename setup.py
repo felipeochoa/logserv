f939c71f136495d26d4a88d7b43474a5acf8a5bf
Felipe <user@server>
2013-10-11 01:38:48 -0500

    renamed src-> logserv

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='logserv',
+      version='1.0',
+      description='Python Socket Logging Server',
+      author='Felipe Ochoa',
+      url='http://www.python.org/sigs/distutils-sig/',
+      packages=['logserv', 'logserv.test'],
+     )
